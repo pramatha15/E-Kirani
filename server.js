f4d5caa6a51a502e9b8b01358fcb477328faf492
Pramatha Devangad <pramathand@gmail.com>
2026-01-19 19:27:32 +0530

    Initialize Express server with routes and middleware

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/products.js";
+import orderRoutes from "./routes/orders.js";
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.listen(5000, () => {
+  console.log("Backend running on http://localhost:5000");
+});
